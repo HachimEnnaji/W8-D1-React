ac441d7f2189f25c558b3bfd5fa29ef5c6e7134f
HachimEnnaji <ashennaji@hotmail.it>
2023-12-18 16:53:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import ButtonComponent from "./components/ButtonComponent";
+import ImageComponent from "./components/ImageComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ImageComponent
+          src="https://images.unsplash.com/photo-1702611120121-e03dafc14150?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          className="App-logo"
+          alt="dog"
+        />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          <ButtonComponent buttonText="Cliccami!" className="buttonClick" />
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
